99ed0df48a92b9af74497022cc94c05e9effc882
AgusDiPietro <agusdipietro@live.com.ar>
2023-12-06 19:15:55 +0100

    Adding Pc and player turns, functions and eventlisteners

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -17,7 +17,9 @@ const btnAsk = document.querySelector('#btnAsk');
 const btnStop = document.querySelector('#btnStop');
 const btnNew = document.querySelector('#btnNew');
 
-const HTMlpoints = document.querySelectorAll('small')
+const divComputerCards = document.querySelector('#computer-cards');
+const divPlayerCards = document.querySelector('#player-cards');
+const HTMlpoints = document.querySelectorAll('small');
 
 const createDeck = () => {
     
@@ -32,7 +34,6 @@ const createDeck = () => {
             deck.push( spe + tipe);
         }
     }
-
     deck = _.shuffle (deck);
     console.log(deck)
     return deck;
@@ -44,9 +45,7 @@ const askCard = () => {
 
     if (deck.length === 0 ) {
         throw 'No more cards in the deck';
-
     }
-
     const card = deck.pop();
     console.log(deck);
     console.log(card);
@@ -59,8 +58,31 @@ const cardValue = (card) => {
     return (isNaN (value ) ) ?
             (value === 'A') ? 11: 10
             : value *1;
-
 }
+
+//Pc turn
+const pcTurn = (minPoints) => {
+
+    do{
+
+    const card = askCard();
+
+    pcPoints = pcPoints + cardValue(card);
+    HTMlpoints[1].innerText = pcPoints;
+
+    // <!-- <img class="carta" src="assets/cartas/cartas/2S.png"> -->
+    const cardImage = document.createElement('img');
+    cardImage.src = `assets/cartas/cartas/${card}.png`;
+    cardImage.classList.add('card');
+
+    divComputerCards.append(cardImage);
+
+    if(playerPoints > 21) {
+        break;
+    }
+
+    } while ( (pcPoints < minPoints) && (minPoints <= 21));
+} 
 
 const value = cardValue (askCard());
 console.log([value]);
@@ -73,7 +95,30 @@ btnAsk.addEventListener('click', () => {
     playerPoints = playerPoints + cardValue(card);
     HTMlpoints[0].innerText = playerPoints;
 
-    
+    // <!-- <img class="carta" src="assets/cartas/cartas/2S.png"> -->
+    const cardImage = document.createElement('img');
+    cardImage.src = `assets/cartas/cartas/${card}.png`;
+    cardImage.classList.add('card');
+
+    divPlayerCards.append(cardImage);
+
+    if(playerPoints > 21) {
+        console.warn('You lost');
+        btnAsk.disabled = true;
+        pcTurn(playerPoints);
+
+    }else if(playerPoints === 21) {
+        console.warn('21 great!');
+        btnAsk.disabled = true;
+        pcTurn(playerPoints);
+      }
+});
+
+btnStop.addEventListener('click', () =>{
+    btnAsk.disabled = true;
+    btnStop.disabled = true;
+
+    pcTurn(playerPoints);
 
 
 })
